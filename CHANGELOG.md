8f4a884ae42fba8f84b06a9565f8ae71c608f8e8
jt <jt@nclouds.com>
2014-05-30 16:17:06 -0700

    testing blah1

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of blah1
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'blah1'
+maintainer       'nclouds.com'
+maintainer_email 'jt@nclouds.com'
+license          'Apache 2.0'
+description      'Installs/Configures blah1'
+long_description 'Installs/Configures blah1'
+version          '0.1.0'
+
+depends "apt"
